fad465410a7d6f984394f29ef55a72f02c3309b9
Junior Araujo Sistemas <junior.araujo21@yahoo.com.br>
2026-01-21 13:51:44 -0300

    Ajuste de dependências para o Render

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import aliased
 from werkzeug.utils import secure_filename
 from datetime import datetime
-from flask_login import login_required, current_user
 
 app = Flask(__name__)
 app.secret_key = "230808Deus#"
